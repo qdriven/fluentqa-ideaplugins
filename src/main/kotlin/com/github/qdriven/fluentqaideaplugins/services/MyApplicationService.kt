5ac7f18892276e8ced40a2104c50d6fcbf4a951b
GitHub Action <action@github.com>
2022-07-07 05:42:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/qdriven/fluentqaideaplugins/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.qdriven.fluentqaideaplugins.services
+
+import com.github.qdriven.fluentqaideaplugins.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/qdriven/fluentqaideaplugins/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qdriven/fluentqaideaplugins/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.qdriven.fluentqaideaplugins.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qdriven.fluentqaideaplugins.MyBundle
 
 class MyProjectService(project: Project) {
 
